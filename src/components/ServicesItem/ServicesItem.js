ce3517dfb793b6b63e386dd9272e848c04d77c20
Abu Salek <bhairabbari330@gmail.com>
2021-10-04 18:49:44 +0600

    add price on every package

--- src/components/ServicesItem/ServicesItem.js
+++ src/components/ServicesItem/ServicesItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ServicesItem.css';
 const ServicesItem = (props) => {
-    const {name, img, subject, teacher, seats, description} = props.item
+    const {name, img, subject, teacher, seats, description, fee} = props.item
     return (
         <div className="col">
             <div className="card h-100">
@@ -9,6 +9,7 @@ const ServicesItem = (props) => {
                 <div className="card-body">
                     <h5 className="card-title">{name}</h5>
                     <p><strong>Total Seats: </strong>{seats}</p>
+                    <p><strong>Monthly fee: </strong>{fee}TK</p>
                     <p><strong>Teacher: </strong>{teacher}</p>
                     <p><strong>Subject: </strong>{subject}</p>
                     <p className="card-text lead"><strong>Description:</strong> {description}</p>
